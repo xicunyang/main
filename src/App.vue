9873382c60a59e6950d507579e22c60276637457
杨喜存 <xicunyang@163.com>
2020-05-18 13:50:57 +0800

    feature: 修改start.sh

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="main">
+    这是主应用文字
+    <br>
+    <button @click="changeView('/one')">子应用one</button>
+    &nbsp;
+    <button @click="changeView('/two')">子应用two</button>
+    <br>
+    <button @click="changeState('1')">修改state = 1</button>
+    &nbsp;
+    <button @click="changeState('2')">修改state = 2</button>
+    <hr>
+    <div id="micro-view">
+
+    </div>
   </div>
 </template>
 
@@ -12,12 +23,22 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  methods: {
+    changeView(who){
+      window.history.pushState(null,who,who)
+    },
+    changeState(value){
+      this.$actions.setGlobalState({
+        mt: value
+      });
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#main {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,127 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+// 导入qiankun
+import {
+    registerMicroApps, // 注册子应用
+    runAfterFirstMounted, // 当地一个子应用装载完毕
+    setDefaultMountApp, // 设置默认装载的子应用
+    initGlobalState, // 微前端之间的通信
+    start, // 启动
+} from 'qiankun'
+
+let app = null
+
+function render({appContetnt} = {}) {
+    if (!app) {
+        // 新建app
+        console.log('新建app渲染');
+        app = new Vue({
+            el: '#container',
+            data() {
+                return {
+                    content: appContetnt
+                }
+            },
+            render(h) {
+                return h(App, {
+                    props: {
+                        content: this.content
+                    }
+                })
+            }
+        })
+    } else {
+        // 已有app
+        console.log('已有app渲染');
+        app.content = appContetnt
+    }
+}
+
+// 渲染主应用
+render()
+
+// 注册子应用
+registerMicroApps([
+        {
+            name: 'one',
+            entry: '//localhost:6661',
+            container: '#micro-view',
+            activeRule: '/one',
+            props: {
+                msg: {
+                    data: {
+                        mt: 'you are one'
+                    }
+                },
+                fn:{
+                    show(msg){
+                        console.log('one:',msg);
+                    }
+                }
+            }
+        },
+        {
+            name: 'two',
+            entry: '//localhost:6662',
+            container: '#micro-view',
+            activeRule: '/two',
+            props: {
+                msg: {
+                    data: {
+                        mt: 'you are two'
+                    }
+                },
+                fn:{
+                    show(msg){
+                        console.log('two:',msg);
+                    }
+                }
+            }
+        }
+    ],
+    {
+        beforeLoad: [
+            app => {
+                console.log('beforeLoad');
+            }
+        ],
+        beforeMount: [
+            app => {
+                console.log('beforeMount');
+            }
+        ],
+        beforeUnmount: [
+            app => {
+                console.log('beforeUnmount');
+            }
+        ],
+        afterUnmount: [
+            app => {
+                console.log('afterUnmount');
+            }
+        ]
+    })
+
+// 通讯
+const actions = initGlobalState({
+    mt: 'init'
+})
+actions.onGlobalStateChange((state,prev)=>{
+    console.log('main state change',state);
+})
+Vue.prototype.$actions = actions
+
+setDefaultMountApp('one')
+
+// 第一个子应用加载完毕后回调
+runAfterFirstMounted(()=>{
+  console.log('第一个子应用加载完毕后的回调');
+})
+
+// 启动微前端
+start()
+
+// new Vue({
+//   render: function (h) { return h(App) },
+// }).$mount('#app')
